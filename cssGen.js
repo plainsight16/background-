9762a542c8178f9c033c575b10b77dd16c74b4cf
plainsight16 <timmybaks2002@gmail.com>
2020-07-21 14:27:16 +0100

    adding some files

---
+++ cssGen.js
@@ -0,0 +1,20 @@
+var header= document.querySelector("h1");
+var body= document.querySelector("body");
+var css= document.querySelector("h3");
+var color1=document.querySelector(".color1");
+var color2=document.querySelector(".color2");
+
+function changeColor(){
+	body.style.background=
+	"linear-gradient(to right, "
+	+ color1.value
+	+ ", "
+	+ color2.value
+	+ ")";
+	css.textContent=body.style.background+";";
+}
+
+
+color1.addEventListener("input", changeColor);
+color2.addEventListener("input",changeColor);
+css.textContent=body.style.background+";";
