f20a5b91bf4b2370cb0318771d9cec943522f64d
plainsight16 <timmybaks2002@gmail.com>
2020-07-23 16:24:43 +0100

    sorry about that

--- cssGen.js
+++ cssGen.js
@@ -1,20 +1,14 @@
 var header= document.querySelector("h1");
 var body= document.querySelector("body");
 var css= document.querySelector("h3");
-var color1=document.querySelector(".color1");
-var color2=document.querySelector(".color2");
+var color1=document.querySelector('.color1');
+var color2=document.querySelector('.color2');
 
 function changeColor(){
-	body.style.background=
-	"linear-gradient(to right, "
-	+ color1.value
-	+ ", "
-	+ color2.value
-	+ ")";
-	css.textContent=body.style.background+";";
+	body.style.background="linear-gradient(to-right, "+color1.value+", "+color2.value+")";
 }
 
 
 color1.addEventListener("input", changeColor);
 color2.addEventListener("input",changeColor);
-css.textContent=body.style.background+";";
+css.document.innerHTML("linear-gradient(to-right, "+color1.value+", "+color2.value+");");
